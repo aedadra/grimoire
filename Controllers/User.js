c67253bb426cd3226544e74088d61331aface538
Clement Moulin <clement.moulin@yahoo.fr>
2023-10-03 00:03:43 +0200

    creation function signup and login in controller auth

--- Controllers/User.js
+++ Controllers/User.js
@@ -5,17 +5,17 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
 exports.signUp = (req, res, next) => {
-bcrypt.hash(req.body.password, 10)
-.then (hash => {
-    const user = new User ({
-        email: req.body.email,
-        password: hash
-    })
-    user.save()
-    .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-    .catch(error => res.status(400).json({ error }));
-})
-.catch (error => res.status(500).json({ error }))
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            })
+            user.save()
+                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+                .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }))
 };
 
 exports.login = (req, res, next) => {
@@ -41,4 +41,4 @@ exports.login = (req, res, next) => {
                 .catch(error => res.status(500).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
- };
+};

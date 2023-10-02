5bd330fdfd6d748b7109c4c23b023a900cb0af2b
Clement Moulin <clement.moulin@yahoo.fr>
2023-10-02 16:44:13 +0200

    Book controller refactoring, add delete, create and update actions

--- Controllers/Book.js
+++ Controllers/Book.js
@@ -1,38 +1,77 @@
 const Book = require('../models/Book');
+const fs = require('fs');
 
-exports.createBook= (req, res, next) => {
-    delete req.body.id;
+exports.createThing = (req, res, next) => {
+    const bookObject = JSON.parse(req.body.book);
+    delete bookObject.id;
+    delete bookObject.userId;
     const book = new Book({
-      ...req.body
+        ...bookObject,
+        userId: req.auth.userId,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
     book.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-      next();
-    };
-  
-    exports.addBook = (req, res, next) => {
-      Book.updateOne({ id: req.params.id }, { ...req.body, id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+        .then(() => { res.status(201).json({ message: 'Objet enregistré !' }) })
+        .catch(error => { res.status(400).json({ error }) })
+};
+
+exports.addBook = (req, res, next) => {
+    Book.updateOne({ id: req.params.id }, { ...req.body, id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet modifié !' }))
         .catch(error => res.status(400).json({ error }));
-    };
-  
-    exports.deleteBook = (req, res, next) => {
-      Book.deleteOne({ id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-        .catch(error => res.status(400).json({ error }));
-    };
-  
-    exports.getOneBook = (req, res, next) => {
-      Book.findOne({ id: req.params.id })
+};
+
+exports.deleteBook = (req, res, next) => {
+    Book.findOne({ id: req.params.id })
+        .then(book => {
+            if (book.userId != req.auth.userId) {
+                res.status(401).json({ message: 'utilisateur non authorisé' });
+            } else {
+                const filename = book.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${filename}`, () => {
+                    Book.deleteOne({ id: req.params.id })
+                        .then(() => { res.status(200).json({ message: 'Objet supprimé !' }) })
+                        .catch(error => res.status(401).json({ error }));
+                });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error });
+        });
+};
+
+exports.getOneBook = (req, res, next) => {
+    Book.findOne({ id: req.params.id })
         .then(Book => res.status(200).json(Book))
         .catch(error => res.status(404).json({ error }));
-        next()
-    };
-  
-  exports.getAllBooks = (req, res, next) => {
+    next()
+};
+
+exports.getAllBooks = (req, res, next) => {
     Book.find()
-      .then(Books => res.status(200).json(Books))
-      .catch(error => res.status(400).json({ error }));
-      next();
-  };
+        .then(Books => res.status(200).json(Books))
+        .catch(error => res.status(400).json({ error }));
+    next();
+};
+
+exports.modifyBook = (req, res, next) => {
+    const bookObject = req.file ? {
+        ...JSON.parse(req.body.book),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+
+    delete bookObject.userId;
+    Book.findOne({ id: req.params.id })
+        .then((book) => {
+            if (book.userId != req.auth.userId) {
+                res.status(401).json({ message: 'utilisateur non autorisé' });
+            } else {
+                Book.updateOne({ id: req.params.id }, { ...bookObject, id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'Objet modifié!' }))
+                    .catch(error => res.status(401).json({ error }));
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({ error });
+        });
+};

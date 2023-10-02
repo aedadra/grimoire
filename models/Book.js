1df04ffff782e51595c234f04a178466e6c0391c
Clement Moulin <clement.moulin@yahoo.fr>
2023-10-03 00:08:50 +0200

    add informations of model book that you can see on the website

--- models/Book.js
+++ models/Book.js
@@ -1,4 +1,4 @@
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 
 const bookSchema = mongoose.Schema({
     userId: { type: String, required: true },
@@ -7,7 +7,7 @@ const bookSchema = mongoose.Schema({
     imageUrl: { type: String, required: true },
     year: { type: Number, required: true },
     genre: { type: String, required: true },
-    ratings: [{ 
+    ratings: [{
         userId: { type: String, required: true },
         grade: { type: Number, required: true, min: 0, max: 5 },
     }],

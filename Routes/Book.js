49c7450a7cf55c90793460d1c049ca279ef845fe
Clement Moulin <clement.moulin@yahoo.fr>
2023-10-03 00:07:55 +0200

    updates routes for login and api

--- Routes/Book.js
+++ Routes/Book.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const BookController = require('../Controllers/Book');
+const auth = require('../Middlewares/auth');
+const multer = require('../Middlewares/multer-config')
 
 const router = express.Router();
 
-const BookController = require('../Controllers/Book');
-
-router.post('/', BookController.createBook);
-router.put('/:id', BookController.addBook);
-router.delete('/:id', BookController.deleteBook);
-router.get('/:id', BookController.getOneBook);
-router.get('/', BookController.getAllBooks);
+router.post('/', auth, multer, BookController.createBook);
+router.put('/:id', auth, multer, BookController.addBook);
+router.delete('/:id', auth, BookController.deleteBook);
+router.get('/:id', auth, BookController.getOneBook);
+router.get('/', auth, BookController.getAllBooks);
 
 module.exports = router;

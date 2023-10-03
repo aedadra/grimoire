11b233b8b237011d6fdac12d160cfbdf0f6a67ca
Clement Moulin <clement.moulin@yahoo.fr>
2023-10-03 02:03:19 +0200

    debug code

--- Routes/User.js
+++ Routes/User.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const userController = require('../Controllers/User')
 
-router.post('/signup', userController.signup);
+router.post('/signup', userController.signUp);
 router.post('/login', userController.login);
 
 module.exports = router;

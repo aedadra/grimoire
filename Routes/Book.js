459478d2c8d6742f84dccdda19f7c01fa882ba8b
Clement Moulin <clement.moulin@yahoo.fr>
2023-10-05 01:58:25 +0200

    add routes for modification of the rating and get best rated books

--- Routes/Book.js
+++ Routes/Book.js
@@ -5,10 +5,12 @@ const multer = require('../Middlewares/multer-config')
 
 const router = express.Router();
 
+router.get('/:id', BookController.getOneBook);
+router.get('/', BookController.getAllBooks);
 router.post('/', auth, multer, BookController.createBook);
 router.put('/:id', auth, multer, BookController.addBook);
 router.delete('/:id', auth, BookController.deleteBook);
-router.get('/:id', auth, BookController.getOneBook);
-router.get('/', BookController.getAllBooks);
+router.get('/bestrating', BookController.bestRatedBooks);
+router.post('/:id/rating', auth, BookController.rateBook);
 
 module.exports = router;

ac6e527e265808e3e66b67efdd395301bd1a8531
Clement Moulin <clement.moulin@yahoo.fr>
2023-10-08 18:09:07 +0200

    add sharp in the routes

--- Routes/Book.js
+++ Routes/Book.js
@@ -2,15 +2,18 @@ const express = require('express');
 const BookController = require('../Controllers/Book');
 const auth = require('../Middlewares/auth');
 const multer = require('../Middlewares/multer-config')
-
+const sharp = require('../Middlewares/sharp');
 const router = express.Router();
 
-router.get('/:id', BookController.getOneBook);
+
 router.get('/', BookController.getAllBooks);
-router.post('/', auth, multer, BookController.createBook);
-router.put('/:id', auth, multer, BookController.addBook);
-router.delete('/:id', auth, BookController.deleteBook);
 router.get('/bestrating', BookController.bestRatedBooks);
+router.get('/:id', BookController.getOneBook);
+router.post('/', auth, multer, sharp, BookController.createBook);
 router.post('/:id/rating', auth, BookController.rateBook);
+router.put('/:id', auth, multer, sharp, BookController.modifyBook);
+router.delete('/:id', auth, BookController.deleteBook);
+
+
 
 module.exports = router;

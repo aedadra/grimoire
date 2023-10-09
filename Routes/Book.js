ca913f974b5a377b589680ad12503080ea4364f2
Clement Moulin <clement.moulin@yahoo.fr>
2023-10-09 14:37:14 +0200

    indentation code

--- Routes/Book.js
+++ Routes/Book.js
@@ -5,7 +5,6 @@ const multer = require('../Middlewares/multer-config')
 const sharp = require('../Middlewares/sharp');
 const router = express.Router();
 
-
 router.get('/', BookController.getAllBooks);
 router.get('/bestrating', BookController.bestRatedBooks);
 router.get('/:id', BookController.getOneBook);
@@ -13,7 +12,5 @@ router.post('/', auth, multer, sharp, BookController.createBook);
 router.post('/:id/rating', auth, BookController.rateBook);
 router.put('/:id', auth, multer, sharp, BookController.modifyBook);
 router.delete('/:id', auth, BookController.deleteBook);
-
-
 
 module.exports = router;

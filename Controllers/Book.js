ca4b5ae4fb996da9c13dd1201676a38c21c1ea21
Clement Moulin <clement.moulin@yahoo.fr>
2023-10-21 18:25:55 +0200

    last modification

--- Controllers/Book.js
+++ Controllers/Book.js
@@ -4,8 +4,6 @@ const path = require('path');
 
 exports.createBook = (req, res, next) => {
     const bookObject = JSON.parse(req.body.book);
-    delete bookObject._id;
-    delete bookObject._userId;
     const book = new Book({
         ...bookObject,
         userId: req.auth.userId,

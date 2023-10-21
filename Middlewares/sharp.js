f379fc07303acbeb4864c8120b56370c55a568d6
Clement Moulin <clement.moulin@yahoo.fr>
2023-10-21 18:12:46 +0200

    modification of file name

--- Middlewares/sharp.js
+++ Middlewares/sharp.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 module.exports = async (req, res, next) => {
     if (!req.file) {
         return next()
-    }
+    };
 
     try {
         req.file.newFilename = req.file.filename + '.webp';
@@ -13,17 +13,22 @@ module.exports = async (req, res, next) => {
 
         await sharp(req.file.path)
             .resize(500, 650)
-            .webp(90)
-            .toFile(req.file.newFilePath) 
+            .webp({quality:90})
+            .toFile(req.file.newFilePath);
 
         fs.unlink(req.file.path, (error) => {
 			if(error) {
-            return ({error});
+                return error;
             }
 		});
 		next();
 
-    } catch (error) {
-        res.status(403).json({error})
+    } catch {
+        (error) => res.status(400).json(error);
+        fs.unlink(req.file.path, (error) => {
+			if(error) {
+                return error;
+            }
+		});
     }
 }

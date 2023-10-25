d0790b216621c47864325acfd79541c3b137893c
Clement Moulin <clement.moulin@yahoo.fr>
2023-10-25 23:49:12 +0200

    gestion des erreurs et cryptage key

--- Controllers/User.js
+++ Controllers/User.js
@@ -13,7 +13,7 @@ exports.signUp = (req, res, next) => {
             });
             user.save()
                 .then(() => res.status(201).json({ message: 'User created !' }))
-                .catch(error => res.status(400).json(error));
+                .catch(error => res.status(500).json(error));
         })
         .catch(error => res.status(500).json(error))
 };
@@ -22,18 +22,18 @@ exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
-                return res.status(401).json({ message: 'email or password incorrect' });
+                return res.status(400).json({ message: 'email or password incorrect' });
             }
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
-                        return res.status(401).json({ message: 'email or password incorrect' });
+                        return res.status(400).json({ message: 'email or password incorrect' });
                     };
                     res.status(200).json({
                         userId: user._id,
                         token: jwt.sign(
                             { userId: user._id },
-                            'RANDOM_TOKEN_SECRET',
+                            `${process.env.KEY}`,
                             { expiresIn: '24h' }
                         )
                     });
